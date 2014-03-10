667637b8de7d26a8058719abaeb22d4b4ace5ce2
Vitalii Savchuk <esvit666@gmail.com>
2014-03-10 11:26:21 +0200

    Update Table.php

--- src/Bazalt/ORM/Exception/Table.php
+++ src/Bazalt/ORM/Exception/Table.php
@@ -23,7 +23,7 @@ class Table extends Base
     {
         $this->tableName = $tableName;
 
-        parent::__construct($message, $innerEx, $code);
+        parent::__construct($message . ' ' . $tableName, $innerEx, $code);
     }
 
     /**
